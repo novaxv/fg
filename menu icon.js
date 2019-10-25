7f517d50bc5defdc7d89e8f1c11387af88227b6e
novaxv <emily.beast29@gmail.com>
2019-10-25 11:30:27 +0800

    enter

---
+++ menu icon.js
@@ -0,0 +1,23 @@
+mybutton=document.getElementById("myBtn");
+window.onscroll=function(){scrollfunction()};
+
+function scrollfunction(){
+if(document.body.scrollTop>20||document.documentElement.scrollTop>20)
+{
+mybutton.style.display="block";}
+else
+{mybutton.style.display="none";}
+
+
+
+
+
+}
+
+function topFunction(){
+document.body.scrollTop=0;
+document.documentElement.scrollTop=0;
+
+
+
+}
